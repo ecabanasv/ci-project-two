7e580e0b66423b7ba2c4952d09838d5c54e5ffb0
ecabanasv <ecvoracle@gmail.com>
2022-06-11 09:25:27 +0000

    Added player/com choices label text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 /*jshint esversion: 6 */
 
-// Scores initialization
-let comScore = 0,
-    playerScore = 0;
-
-let imgPlayer = document.getElementById("img-player");
-let imgComputer = document.getElementById("img-computer");
-let btnRestart = document.getElementById("btn-restart");
+// Variables for Player and Computer
+let playerScore = 0,
+    imgPlayer = document.getElementById("img-player"),
+    playerScoreText = document.getElementById("player-score"),
+    playerLabel = document.getElementById("player-label"),
+    playerPlays = document.getElementById("player-choice"),
+    comScore = 0,
+    imgComputer = document.getElementById("img-computer"),
+    comScoreText = document.getElementById("com-score"),
+    computerLabel = document.getElementById("computer-label"),
+    computerPlays = document.getElementById("computer-choice"),
+    textResult = document.getElementById("text-result"),
+    btnRestart = document.getElementById("btn-restart");
 
 // Function that gather player and computer choices and call the 
 // function roundWinner to choose the winner and increase score
@@ -16,7 +22,7 @@ function playerChoose() {
     // Computer choice
     let comChoice = computerChoose();
 
-    if (playerScore > 1 || comScore > 1){
+    if (playerScore > 1 || comScore > 1) {
         btnRestart.style.visibility = 'visible'
     }
 
@@ -40,15 +46,17 @@ function computerChoose() {
 // Function check the player choice agaisnt computer for choose the winner and increase its score
 function roundWinner(playerChoice, comChoice) {
     let winnerResult;
-
+    playerPlays.innerText = playerChoice;
+    computerPlays.innerText = comChoice;
     if (playerChoice === comChoice) {
         imgPlayer.src = 'assets/images/' + playerChoice + '-x.png';
         imgComputer.src = 'assets/images/' + comChoice + '-x.png';
+
         winnerResult = 'draw';
     } else {
         switch (playerChoice) {
             case 'rock':
-            
+
                 imgPlayer.src = 'assets/images/rock-x.png';
                 imgComputer.src = 'assets/images/' + comChoice + '-x.png';
                 if (comChoice === 'scissors' || comChoice === 'lizard') {
@@ -104,36 +112,39 @@ function roundWinner(playerChoice, comChoice) {
 // Functions that increases the score of the player/computer 
 function adjustScore() {
     if (arguments[0] === 'player') {
-        document.getElementById("player-score").innerText = ++playerScore;
-        document.getElementById("text-result").innerText = "Player won this round";
+        playerScoreText.innerText = ++playerScore;
+        textResult.innerText = "Player won this round";
     } else if (arguments[0] === 'computer') {
         document.getElementById("com-score").innerText = ++comScore;
-        document.getElementById("text-result").innerText = "Computer won this round";
+        textResult.innerText = "Computer won this round";
     } else {
-        document.getElementById("text-result").innerText = "Draw round";
+        textResult.innerText = "Draw round";
     }
 
-    if (playerScore > comScore){
-        document.getElementById("player-label").style.color = "green";
-        document.getElementById("computer-label").style.color = "red";
-    } else if (playerScore < comScore){
-        document.getElementById("player-label").style.color = "red";
-        document.getElementById("computer-label").style.color = "green"; 
-    } else{
-        document.getElementById("player-label").style.color = "black";
-        document.getElementById("computer-label").style.color = "black";     
+    if (playerScore > comScore) {
+        playerLabel.style.color = "green";
+        computerLabel.style.color = "red";
+    } else if (playerScore < comScore) {
+        playerLabel.style.color = "red";
+        computerLabel.style.color = "green";
+    } else {
+        playerLabel.style.color = "black";
+        computerLabel.style.color = "black";
     }
 }
 
-function restartGame(){
+// Function that reset the game 
+function restartGame() {
     imgPlayer.src = 'assets/images/player.png';
     imgComputer.src = 'assets/images/computer.png';
-    document.getElementById("player-score").innerText = 0;
-    document.getElementById("com-score").innerText = 0;
-    document.getElementById("text-result").innerText = "New game, choose wisely...";
+    playerScoreText.innerText = 0;
+    comScoreText.innerText = 0;
+    textResult.innerText = "New game, choose wisely...";
     playerScore = 0;
     comScore = 0;
-    document.getElementById("player-label").style.color = "black";
-    document.getElementById("computer-label").style.color = "black";
+    playerPlays.innerText = "";
+    computerPlays.innerText = "";
+    playerLabel.style.color = "black";
+    computerLabel.style.color = "black";
     btnRestart.style.visibility = 'hidden';
 }
