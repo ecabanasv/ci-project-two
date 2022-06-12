4ae4cda053bd28707ecd659e5ffe462be319330a
ecabanasv <ecvoracle@gmail.com>
2022-06-12 11:31:58 +0000

    Added sweet alert library and instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,5 +185,5 @@ function restartGame() {
 }
 
 function popInstructions(){
-    Swal.fire({icon: 'info', html: 'Wins who reach 10 points. Game can be reset after 2 points.<br><br>- Combinations -<br>Scissors cuts Paper<br>Paper covers Rock<br>Rock crushes Lizard<br>Lizard poisons Spock<br>Spock smashes Scissors<br>Scissors decapitates Lizard<br>Lizard eats Paper<br>Paper disproves Spock<br>Spock vaporizes Rock<br>Rock crushes Scissors',showCloseButton: true});
+    Swal.fire({title: '<strong>Instructions</strong>', icon: 'info', html: 'Wins who reach 10 points. Game can be reset after 2 points.<br><br>- Combinations -<br>Scissors cuts Paper<br>Paper covers Rock<br>Rock crushes Lizard<br>Lizard poisons Spock<br>Spock smashes Scissors<br>Scissors decapitates Lizard<br>Lizard eats Paper<br>Paper disproves Spock<br>Spock vaporizes Rock<br>Rock crushes Scissors',showCloseButton: true});
 }
